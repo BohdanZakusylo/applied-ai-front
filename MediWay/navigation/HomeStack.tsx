0e410361c3ef1a8ade95962f74c9409007024bff
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2025-05-30 14:07:41 +0200

    Fix merge conflict

--- MediWay/navigation/HomeStack.tsx
+++ MediWay/navigation/HomeStack.tsx
@@ -1,7 +1,6 @@
 //used only for displaying and navigatiion for Home Screens
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Test2 from '../screens/Test2';
-import Profile from '../screens/Profile/Profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,7 +8,7 @@ function HomeStack() {
     return (
         <Tab.Navigator>
             <Tab.Screen name="Test2" component={Test2} />
-            <Tab.Screen name="Profile" component={Profile} />
+
         </Tab.Navigator>
     );
 };

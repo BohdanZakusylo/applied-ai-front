23d8fd2bb2e9f7ae3dc03f5801d2f61c07e8a731
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2025-06-05 14:22:20 +0200

    Add fetch and display user data

--- MediWay/screens/Profile/Profile.tsx
+++ MediWay/screens/Profile/Profile.tsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
 
 const USER = require('../../assets/images/User.png');
 const EDIT = require('../../assets/images/EditButton.png');
 
-export default function ProfileScreen() {
+const ProfileScreen = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [insuranceProvider, setInsuranceProvider] = useState();
+  const [generalPractitioner, setGeneralPractitioner] = useState();
+  const [medicalInformation, setMedicalInformation] = useState();
+
+  useEffect(() => {
+  const fetchUser = async () => {
+    console.log("fetch start");
+
+    try {
+      const response = await fetch("http://172.27.112.1:8000/api/v1/users/profile", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": "Bearer <token>"
+        }
+      });
+
+      console.log("response", response);
+
+      const data = await response.json();
+      console.log("data", data);
+
+      setName(data.name);
+      setEmail(data.email);
+      setInsuranceProvider(data.insurance_provider);
+      setGeneralPractitioner(data.general_practitioner);
+      setMedicalInformation(data.medical_information);
+
+    } catch (err) {
+      console.error("Fetch failed:", err);
+    }
+  };
+
+  fetchUser();
+}, []);
+
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Image source={USER} style={styles.user} resizeMode="contain" />
-      <Text style={styles.name}>JOHN DOE</Text>
+      <Text style={styles.name}>{name}</Text>
 
       <View style={styles.section}>
         <View style={styles.sectionHeader}>
@@ -20,7 +59,7 @@ export default function ProfileScreen() {
 
         <View style={styles.row}>
           <Text style={styles.label}>Email</Text>
-          <Text style={styles.value}>johndoe@email.com</Text>
+          <Text style={styles.value}>{email}</Text>
         </View>
         <View style={styles.row}>
           <Text style={styles.label}>Password</Text>
@@ -38,20 +77,22 @@ export default function ProfileScreen() {
 
         <View style={styles.row}>
           <Text style={styles.label}>Insurance Provider</Text>
-          <Text style={styles.value}>None</Text>
+          <Text style={styles.value}>{insuranceProvider}</Text>
         </View>
         <View style={styles.row}>
           <Text style={styles.label}>General Practitioner</Text>
-          <Text style={styles.value}>None</Text>
+          <Text style={styles.value}>{generalPractitioner}</Text>
         </View>
         <View style={styles.row}>
           <Text style={styles.label}>Medical Information</Text>
-          <Text style={styles.value}>None</Text>
+          <Text style={styles.value}>{medicalInformation}</Text>
         </View>
       </View>
     </ScrollView>
   );
 }
+
+export default ProfileScreen;
 
 const styles = StyleSheet.create({
   container: {
